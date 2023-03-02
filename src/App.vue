f7f9e648c0553a2a562743e6bdaf714bb66352a8
chengjiahui <327127960@qq.com>
2023-03-02 18:27:14 +0800

    Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>{{ count }}</h1>
+    <h1>{{ double }}</h1>
+    <button @click="add">👍+1</button>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+// import { defineComponent } from 'vue';
+// import HelloWorld from './components/HelloWorld.vue';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+// export default defineComponent({
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// });
+import { ref, computed, reactive, toRefs } from "vue";
+interface typeData {
+  count: number
+  add: () => void
+  double: number
+}
+export default {
+  setup() {
+    // const count = ref(0);
+    // const add = () => {
+    //   return count.value++
+    // }
+    // const double = computed(() => {
+    //   return count.value * 2
+    // })
+    // return {
+    //   count,
+    //   add,
+    //   double
+    // };
+    const data: typeData = reactive({
+        count: 0,
+        add: () => {
+          return data.count++
+        },
+        double: computed(() => {
+          return data.count * 2
+        })
+    })
+    const refData = toRefs(data)
+    return {
+      ...refData
+    }
+  },
+};
 </script>
 
 <style>
